ea788f89e4bf69a4b6dcf65c68368c5ac76da787
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-03-26 02:17:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function sum(a, b){
+  return a+b
+
+
+
+}
+ let sum2 = (a, b) >= a + b
+ 
+
+
+function isPositive(number){
+  return number >= 0
+}
+//Arrow Function
+let isPositive2 =(number) => number >= 0
+
+function randomNumber(){
+  return Math.random
+}
+
+let randomNumber = () => Math.random
+
+
+
+// Arrow functions are really important when creating anonymous functions 
+document.addEventListener('click',  ()   => console.log('click'))
